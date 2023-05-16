b576fd53d8e5dfe20312929c39d0d3f559abc356
maddsua <102395937+maddsua@users.noreply.github.com>
2023-05-16 10:22:01 +0300

    Update index.js

--- examples/getAreas/index.js
+++ examples/getAreas/index.js
@@ -1,6 +1,6 @@
 import { getAreas } from "../../dist/models/Address.js";
 
 (async () => {
-	const { success, data, errors } =  await getAreas('');	//	works without an api token for now
+	const { success, data, errors } = await getAreas('');	//	works without api token for now
 	console.log(success ? data : errors);
 })();
